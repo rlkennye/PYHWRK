c659b146747a590562d3904daf46bbed84ec44cf
Ricardo L Kenny <ricardolkenny@gmail.com>
2019-09-09 16:17:40 -0400

    adding all csv files to resource folder

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,13 +18,25 @@
 ## Status: production version
 ##################################################
 
-# Libs
+# First we'll import the os module
+# This will allow us to create file paths across operating systems
+import os
 
-# Own surces
+# Module for reading CSV files
+import csv
+
+# My Functions
+# def 
+
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+with open(csvpath, 'r') as file_handler:
+    lines = file_handler.read()
+    print(lines)
 
 # Begin
 print("Financial Analysis")
 print("---------------------------")
+print("Total Months: ")
 # The total number of months included in the dataset
 # The net total amount of "Profit/Losses" over the entire period
 # The average of the changes in "Profit/Losses" over the entire period
