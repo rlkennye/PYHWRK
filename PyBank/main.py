539cc86ea01d00d107d7942ed98032404502ccc5
Ricardo L Kenny <ricardolkenny@gmail.com>
2019-09-08 22:34:03 -0400

    Print first 2 lines of ouput

--- PyBank/main.py
+++ PyBank/main.py
@@ -22,6 +22,9 @@
 
 # Own surces
 
+# Begin
+print ("Financial Analysis")
+print (----------------------------)
 # The total number of months included in the dataset
 # The net total amount of "Profit/Losses" over the entire period
 # The average of the changes in "Profit/Losses" over the entire period
