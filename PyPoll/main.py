10a9a5895eb4ec271b41623385dab3d97cf20461
Ricardo L Kenny <ricardolkenny@gmail.com>
2019-09-08 22:11:01 -0400

    added header to main.py to include settings

---
+++ PyPoll/main.py
@@ -0,0 +1,29 @@
+##################################################
+## {This program computes the following info on a dataset:
+## The total number of months included in the dataset
+## The net total amount of "Profit/Losses" over the entire period
+## The average of the changes in "Profit/Losses" over the entire period
+## The greatest increase in profits (date and amount) over the entire period
+## The greatest decrease in losses (date and amount) over the entire period}
+##################################################
+## {License_info}
+##################################################
+## Author: Ricardo L Kenny
+## Copyright: Copyright {2019}, {main.py}
+## Credits: BootCampSpot Team
+## License: N/A
+## Version: Python 3.7.3
+## Mmaintainer: {maintainer}
+## Email: RicardoLKenny@gmail.com
+## Status: production version
+##################################################
+
+# Libs
+
+# Own surces
+
+# The total number of months included in the dataset
+# The net total amount of "Profit/Losses" over the entire period
+# The average of the changes in "Profit/Losses" over the entire period
+# The greatest increase in profits (date and amount) over the entire period
+# The greatest decrease in losses (date and amount) over the entire period
