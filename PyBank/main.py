bbcc60f20c2b7fc2a12ba459f55be6bd2fa89804
Ricardo Kenny <ricardolkenny@gmail.com>
2019-09-15 23:21:38 -0400

    Final changes for Python homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,7 +6,7 @@
 ## The greatest increase in profits (date and amount) over the entire period
 ## The greatest decrease in losses (date and amount) over the entire period}
 ##################################################
-## {License_info}
+## {License_info} N/A
 ##################################################
 ## Author: Ricardo L Kenny
 ## Copyright: Copyright {2019}, {main.py}
@@ -17,28 +17,111 @@
 ## Email: RicardoLKenny@gmail.com
 ## Status: production version
 ##################################################
-
-# First we'll import the os module
-# This will allow us to create file paths across operating systems
+## Input File(s): 
+##      ../Resources/budget_data.csv
+##################################################
+## Output File(s): 
+##       main_output.txt
+##################################################
+## imports
 import os
-
-# Module for reading CSV files
 import csv
+import sys
+##################################################
+## Variables
+row_count = 0
+column_count = 0
+diff_count = 0
+avg_change = 0
+average_change = 0
+top_row = 0
+bottom_row_minus_top = 0
+saved_difference = 0
+profit_losses = 0
+my_dict = {}
+############################
 
-# My Functions
-# def 
+##############################
+## Main Program begins here ##
+##############################
 
+# Specify the file to read from
 csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
-with open(csvpath, 'r') as file_handler:
-    lines = file_handler.read()
-    print(lines)
 
-# Begin
+# Get total months with sum function
+with open(csvpath, 'r') as file_handler:
+    row_count = sum(1 for row in file_handler)
+
+# Get total count of column Profit - Losses 
+with open(csvpath, newline='') as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+
+    # Add up all Profit - Losses
+    for row in csvreader:
+        if column_count == 0: # first time in the loop
+            profit_losses = int(row[1])
+        else:
+            profit_losses = int(profit_losses) + int(row[1])
+        column_count += 1 # next row to be added
+        
+# Get total count of average change
+with open(csvpath, newline='') as csvfile:
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+
+    # Compute avg_change
+    for row in csvreader:
+
+        if diff_count == 0:
+            top_row = int(row[1])
+        else:
+            bottom_row_minus_top = (int(row[1])) - top_row
+            top_row = int(row[1])
+            saved_difference = saved_difference + bottom_row_minus_top
+            my_dict[bottom_row_minus_top] = (row[0])
+            avg_change = (saved_difference / (row_count - 2))
+        diff_count += 1
+
+# round to 2 decimal points       
+average_change = round(avg_change, 2)    
+    
+# Print to Screen
+print(" ")
 print("Financial Analysis")
 print("---------------------------")
-print("Total Months: ")
-# The total number of months included in the dataset
-# The net total amount of "Profit/Losses" over the entire period
-# The average of the changes in "Profit/Losses" over the entire period
-# The greatest increase in profits (date and amount) over the entire period
-# The greatest decrease in losses (date and amount) over the entire period
+print("Total Months: " + str(row_count - 1)) 
+print("Total: $" + str(profit_losses))
+print("Average  Change: $" + str(average_change)) 
+maximum = max(my_dict)
+print("Greatest Increase in Profits: ", my_dict[maximum], maximum)
+minimum = min(my_dict)
+print("Greatest Decrease in Profits: ", my_dict[minimum], minimum)
+print(" ")
+
+# Print to Text file
+orig_stdout = sys.stdout
+f = open('main_output.txt', 'w')
+sys.stdout = f
+
+print(" ")
+print("Financial Analysis")
+print("---------------------------")
+print("Total Months: " + str(row_count - 1)) 
+print("Total: $" + str(profit_losses))
+print("Average  Change: $" + str(average_change)) 
+maximum = max(my_dict)
+print("Greatest Increase in Profits: ", my_dict[maximum], maximum)
+minimum = min(my_dict)
+print("Greatest Decrease in Profits: ", my_dict[minimum], minimum)
+print(" ")
+
+sys.stdout = orig_stdout
+f.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,12 +1,12 @@
 ##################################################
 ## {This program computes the following info on a dataset:
-## The total number of months included in the dataset
-## The net total amount of "Profit/Losses" over the entire period
-## The average of the changes in "Profit/Losses" over the entire period
-## The greatest increase in profits (date and amount) over the entire period
-## The greatest decrease in losses (date and amount) over the entire period}
+##	The total number of votes cast
+##	A complete list of candidates who received votes
+##	The percentage of votes each candidate won
+##	The total number of votes each candidate won
+##	The winner of the election based on popular vote.
 ##################################################
-## {License_info}
+## {License_info} N/A
 ##################################################
 ## Author: Ricardo L Kenny
 ## Copyright: Copyright {2019}, {main.py}
@@ -17,13 +17,98 @@
 ## Email: RicardoLKenny@gmail.com
 ## Status: production version
 ##################################################
+## Input File(s): 
+##      ../Resources/election_data.csv
+##################################################
+## Output File(s): 
+##       main_output.txt
+##################################################
+## imports
+import os
+import csv
+import sys
+##################################################
+## Variables
+C_Khan = 0
+C_Correy = 0
+C_Li = 0
+C_OTooley = 0
+my_dict = {}
+##################################################
+# Define function 
+def perc_count(C_Name, C_Votes, Total_Votes):
+    perc = float(C_Votes / (Total_Votes)) * 100
+    new_perc = round(perc, 3)
+    print(C_Name + str(new_perc) + "%" + "  " +"(" + str(C_Votes) + ")")
 
-# Libs
+##############################
+## Main Program begins here ##
+##############################
 
-# Own surces
+# Specify the file to read from
+csvpath = os.path.join('..', 'Resources', 'election_data.csv')
 
-# The total number of months included in the dataset
-# The net total amount of "Profit/Losses" over the entire period
-# The average of the changes in "Profit/Losses" over the entire period
-# The greatest increase in profits (date and amount) over the entire period
-# The greatest decrease in losses (date and amount) over the entire period
+# Get total votes cast with sum function
+with open(csvpath, 'r') as file_handler:
+    row_count = sum(1 for row in file_handler)
+
+# Get total votes for each Candidate 
+with open(csvpath, newline='') as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+
+    # Add up all Profit - Losses
+    for row in csvreader:
+        if (row[2]) == "Khan":
+            C_Khan += 1
+        elif (row[2]) == "Correy":
+            C_Correy += 1
+        elif (row[2]) == "Li":
+            C_Li += 1
+        else:
+            C_OTooley += 1
+my_dict = {C_Khan:'Khan',C_Correy:'Correy',C_Li:'Li',C_OTooley:'O\'Tooley'}
+
+# Print to Screen
+print(" ")
+print("Election Results")
+print("---------------------------")
+print("Total Votes: " + str(row_count - 1)) 
+print("---------------------------")
+perc_count("Khan: ",C_Khan,row_count)
+perc_count("Correy: ",C_Correy,row_count)
+perc_count("Li: ",C_Li,row_count)
+perc_count("O\'Tooley: ",C_OTooley,row_count)
+print("---------------------------")
+maximum = max(my_dict)
+print("Winner: ", my_dict[maximum] )
+print("---------------------------")
+print(" ")
+
+# Print to Text file
+orig_stdout = sys.stdout
+f = open('main_output.txt', 'w')
+sys.stdout = f
+
+print(" ")
+print("Election Results")
+print("---------------------------")
+print("Total Votes: " + str(row_count - 1)) 
+print("---------------------------")
+perc_count("Khan: ",C_Khan,row_count)
+perc_count("Correy: ",C_Correy,row_count)
+perc_count("Li: ",C_Li,row_count)
+perc_count("O\'Tooley: ",C_OTooley,row_count)
+print("---------------------------")
+maximum = max(my_dict)
+print("Winner: ", my_dict[maximum] )
+print("---------------------------")
+print(" ")
+
+
+sys.stdout = orig_stdout
+f.close()
